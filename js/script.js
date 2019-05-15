8ab4a41673b8978823f0129e78630de5a68c709b
MorozOksana <oksanamoroz98@gmail.com>
2019-05-15 12:15:39 +0300

    Fixed links.

--- js/script.js
+++ js/script.js
@@ -34,7 +34,3 @@ $(".toggleMenu").click(function(e) {
    e.preventDefault();
    $(".nav").toggle();
 });
-if (ww < 800) {
-   $(".toggleMenu").css("display", "inline-block");
-   $(".nav").hide();
-}
